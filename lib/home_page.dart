8c6a7238dc149f49767cf7a1cd47a6a4f60f8426
Rahmat Aji <rahmataji@Rahmats-MacBook-Air.local>
2023-08-10 16:27:24 +0700

    add tabbar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+List<String> tabTitles = <String>['Home', 'Movies', 'Favourites'];
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
@@ -10,29 +12,83 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: false,
-        backgroundColor: const Color.fromARGB(255, 193, 106, 208),
-        leading: const Icon(Icons.home),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.settings_rounded),
-            onPressed: () {},
-          )
-        ],
-        title: const Text(
-          "Flutter AppBar",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+    const int tabsCount = 1;
+
+    return DefaultTabController(
+      length: tabsCount,
+      initialIndex: 0,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          backgroundColor: const Color.fromARGB(255, 193, 106, 208),
+          leading: const Icon(Icons.menu),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.settings_rounded),
+              onPressed: () {},
+            )
+          ],
+          title: const Text(
+            "Flutter AppBar",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          notificationPredicate: (ScrollNotification notification) {
+            return notification.depth == 1;
+          },
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(Icons.cloud_outlined),
+                text: tabTitles[0],
+              ),
+              Tab(
+                icon: const Icon(Icons.beach_access_sharp),
+                text: tabTitles[1],
+              ),
+              Tab(
+                icon: const Icon(Icons.favorite_outline),
+                text: tabTitles[2],
+              ),
+            ],
           ),
         ),
-      ),
-      body: const Center(
-        child: Text(
-          "Flutter AppBar / Practice 1",
-          style: TextStyle(
-              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+        body: TabBarView(
+          children: <Widget>[
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${tabTitles[0]} $index'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${tabTitles[1]} $index'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${tabTitles[2]} $index'),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
